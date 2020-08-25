1b4a5812b813205aeef166479875e2f1292de730
chiang67 <sean.chiang@mail.utoronto.ca>
2020-08-24 20:53:20 -0400

    Change rendering to draw scene while traversing the SPTree

--- src/graphics3D/game.py
+++ src/graphics3D/game.py
@@ -1,6 +1,5 @@
 import math
 import sys
-from random import randint
 
 import numpy as np
 import pygame
@@ -8,8 +7,6 @@ from pygame.locals import *
 from shapely.geometry import Point
 
 from graphics3D.camera.groundcamera import GroundCamera
-from graphics3D.wireframe.wall_builder import WallBuilder
-from graphics3D.wireframe.wireframe_builder import WireframeBuilder
 from sptree.sp_tree import Perspective
 
 
@@ -33,6 +30,7 @@ class Game:
 
     def __init__(self, sptree):
         pygame.init()
+        sptree.build_walls()
         self.sptree = sptree
         self.fps = 144
         self.fpsClock = pygame.time.Clock()
@@ -43,10 +41,6 @@ class Game:
         self.wireframes = []
 
     def run(self):
-
-        node_radius = 3
-        line_radius = 5
-        node_color = (255, 255, 255)
 
         pygame.key.set_repeat(10, 10)  # Required for continuous motion when key is held down
 
@@ -81,42 +75,67 @@ class Game:
 
             # Draw once when the camera is physically moved, and only once
             if camera_moved:
-
                 self.screen.fill((0, 0, 0))
-
-                camera_location_3d = self.camera.coords.change_to_global_basis(np.array([0, 0, 0, 1]))
-                camera_location_2d = Point(camera_location_3d[0], camera_location_3d[2])
-
-                render_order = self._get_render_order(self.sptree, camera_location_2d)
-
-                for wf in render_order:
-
-                    for n1, n2, edge_color in wf.get_global_edges():
-                        pr_line_start = self.get_camera_visible_projection(n1)
-                        pr_line_end = self.get_camera_visible_projection(n2)
-                        if pr_line_start is not None and pr_line_end is not None:
-                            pygame.draw.line(self.screen, edge_color, pr_line_start, pr_line_end, line_radius)
-
-                    for node in wf.get_global_nodes():
-                        pr_pt = self.get_camera_visible_projection(node)
-                        if pr_pt is not None:
-                            pygame.draw.circle(self.screen, node_color, pr_pt, node_radius)
-
-                    for mesh in wf.get_global_meshes():
-                        mesh_nodes = []
-                        for node in mesh[0]:
-                            pr_pt = self.get_camera_visible_projection(node)
-                            if pr_pt is not None:
-                                mesh_nodes.append(pr_pt)
-                        if len(mesh_nodes) > 2:
-                            pygame.draw.polygon(self.screen, mesh[1], mesh_nodes)
-
+                self._draw_walls()
                 camera_moved = False
 
             pygame.display.flip()
             self.fpsClock.tick(self.fps)
 
-    def get_camera_visible_projection(self, node):
+    def _draw_walls(self):
+        camera_location_3d = self.camera.coords.change_to_global_basis(np.array([0, 0, 0, 1]))
+        camera_location_2d = Point(camera_location_3d[0], camera_location_3d[2])
+        self._painters_algorithm(self.sptree.root, camera_location_2d)
+
+    def _painters_algorithm(self, cur_node, camera_location):
+        if cur_node is None:
+            return
+
+        elif cur_node.is_leaf():
+            self._draw_wall_at_node(cur_node)
+
+        elif Perspective.classify(camera_location, cur_node.wall.base, self.sptree.bounding_box) == Perspective.FRONT:
+            self._painters_algorithm(cur_node.right, camera_location)
+            self._draw_wall_at_node(cur_node)
+            self._painters_algorithm(cur_node.left, camera_location)
+
+        elif Perspective.classify(camera_location, cur_node.wall.base, self.sptree.bounding_box) == Perspective.BACK:
+            self._painters_algorithm(cur_node.left, camera_location)
+            self._draw_wall_at_node(cur_node)
+            self._painters_algorithm(cur_node.right, camera_location)
+
+        else:
+            self._painters_algorithm(cur_node.left, camera_location)
+            self._painters_algorithm(cur_node.right, camera_location)
+
+    def _draw_wall_at_node(self, node):
+        node_radius = 3
+        line_radius = 5
+        node_color = (255, 255, 255)
+
+        wf = node.wall
+
+        for n1, n2, edge_color in wf.get_global_edges():
+            pr_line_start = self._get_camera_visible_projection(n1)
+            pr_line_end = self._get_camera_visible_projection(n2)
+            if pr_line_start is not None and pr_line_end is not None:
+                pygame.draw.line(self.screen, edge_color, pr_line_start, pr_line_end, line_radius)
+
+        for node in wf.get_global_nodes():
+            pr_pt = self._get_camera_visible_projection(node)
+            if pr_pt is not None:
+                pygame.draw.circle(self.screen, node_color, pr_pt, node_radius)
+
+        for mesh in wf.get_global_meshes():
+            mesh_nodes = []
+            for node in mesh[0]:
+                pr_pt = self._get_camera_visible_projection(node)
+                if pr_pt is not None:
+                    mesh_nodes.append(pr_pt)
+            if len(mesh_nodes) > 2:
+                pygame.draw.polygon(self.screen, mesh[1], mesh_nodes)
+
+    def _get_camera_visible_projection(self, node):
         # Camera space point
         cs_pt = self.camera.coords.change_to_local_basis(node)
 
@@ -136,65 +155,3 @@ class Game:
             return rs_x, rs_y
 
         return None
-
-    @staticmethod
-    def _get_render_order(sptree, camera_location):
-        render_order = []
-        Game._get_render_order_helper(sptree.root, sptree.bounding_box, camera_location, render_order)
-        return render_order
-
-    @staticmethod
-    def _get_render_order_helper(cur_node, bounding_box, camera_location, render_order):
-        if cur_node is None:
-            return
-
-        elif cur_node.is_leaf():
-            Game._get_walls_at_node(cur_node, render_order)
-
-        elif Perspective.classify(camera_location, cur_node.lines[0], bounding_box) == Perspective.FRONT:
-            Game._get_render_order_helper(cur_node.right, bounding_box, camera_location, render_order)
-            Game._get_walls_at_node(cur_node, render_order)
-            Game._get_render_order_helper(cur_node.left, bounding_box, camera_location, render_order)
-
-        elif Perspective.classify(camera_location, cur_node.lines[0], bounding_box) == Perspective.BACK:
-            Game._get_render_order_helper(cur_node.left, bounding_box, camera_location, render_order)
-            Game._get_walls_at_node(cur_node, render_order)
-            Game._get_render_order_helper(cur_node.right, bounding_box, camera_location, render_order)
-
-        else:
-            Game._get_render_order_helper(cur_node.left, bounding_box, camera_location, render_order)
-            Game._get_render_order_helper(cur_node.right, bounding_box, camera_location, render_order)
-
-    @staticmethod
-    def _get_walls_at_node(node, render_order):
-        for line in node.lines:
-            render_order.append(WallBuilder(line, 50, (255, 255, 255), (0, 255, 0)).build())
-
-
-def get_rand_color():
-    return randint(0, 255), randint(0, 255), randint(0, 255)
-
-
-if __name__ == "__main__":
-    white_edge = (255, 255, 255)
-    red_edge = (255, 0, 0)
-    green_edge = (0, 255, 0)
-    blue_edge = (0, 0, 255)
-
-    # surface_color = (255, 255, 0)
-
-    axis_builder = WireframeBuilder()
-    axis_builder.add_node(0, 0, 0)
-    axis_builder.add_node(40, 0, 0)
-    axis_builder.add_node(0, 40, 0)
-    axis_builder.add_node(0, 0, 40)
-    axis_builder.add_edges(((0, 1, red_edge), (0, 2, green_edge), (0, 3, blue_edge)))
-    axis = axis_builder.build()
-
-    cube_builder = WireframeBuilder()
-    cube_builder.add_nodes([(x, y, z) for x in (50, 200) for y in (-50, 100) for z in (50, 200)])
-    cube_builder.add_mesh(((0, 4, 6, 2), get_rand_color()))
-    cube_builder.add_edges([(n, n + 4, white_edge) for n in range(0, 4)])
-    cube_builder.add_edges([(n, n + 1, white_edge) for n in range(0, 8, 2)])
-    cube_builder.add_edges([(n, n + 2, white_edge) for n in (0, 1, 4, 5)])
-    cube = cube_builder.build()
