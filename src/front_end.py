d06559fd20f1582035d9445fbea1c67bd593e285
chiang67 <sean.chiang@mail.utoronto.ca>
2020-11-08 20:29:29 -0500

    Update frontend to support command line arguments

--- src/front_end.py
+++ src/front_end.py
@@ -1,3 +1,5 @@
+import sys
+
 from shapely.geometry import box
 
 from graphics2D.graphics_2d import Graphics2D
@@ -6,17 +8,40 @@ from sptree.sp_tree import SPTree
 from wall.wall_creator import create_walls
 
 if __name__ == "__main__":
-    width = 768
-    height = 768
+    """
+    Arguments:
+        bb_width - width of bounding box for all walls
+        bb_height - height of bounding box for all walls
+        num_walls - number of walls to add to the scene
+        min_wall_height - minimum height of a wall
+        max_wall_height - maximum height of a wall
+        graphics_type - '3D' for 3D graphics and '2D' for top down view (no quotes)
+    """
+    arg_list = sys.argv
+    if len(arg_list) != 7:
+        print("Usage: \"python3 front_end.py bb_width bb_height num_walls min_wall_height max_wall_height graphics_type")
+        exit(1)
+
+    width = int(arg_list[1])
+    height = int(arg_list[2])
+    num_walls = int(arg_list[3])
+    min_wall_height = int(arg_list[4])
+    max_wall_height = int(arg_list[5])
+
     b_box = box(0, 0, width, height)  # Bounding box in the first quadrant with above width and height
 
-    num_walls = 10
-    lines = create_walls(b_box, num_walls, 20, 40)
+    lines = create_walls(b_box, num_walls, min_wall_height, max_wall_height)
 
     sptree = SPTree(lines, b_box)
 
-    game = Graphics3D(sptree)
-    game.run()
+    graphics_type = arg_list[6]
 
-    display = Graphics2D(sptree)
-    display.run()
+    if graphics_type == '2D':
+        display = Graphics2D(sptree)
+        display.run()
+    elif graphics_type == '3D':
+        game = Graphics3D(sptree)
+        game.run()
+    else:
+        print("Unrecognized graphics type. Use '2D' or '3D' for parameter graphics_type.")
+        exit(1)
